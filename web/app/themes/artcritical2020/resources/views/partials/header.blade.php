72a4503845b98bd81ac29aa8c6f6906da8e4b950
Francois Huyghe <francois@13milliseconds.com>
2021-07-20 13:02:17 -0400

    remove menu javascript

--- web/app/themes/artcritical2020/resources/views/partials/header.blade.php
+++ web/app/themes/artcritical2020/resources/views/partials/header.blade.php
@@ -46,92 +46,121 @@
       @php $options = unserialize(get_option('top_menu_options')); @endphp
       <div id="menu">
         <ul>
-          <li class="ddheader color_thelist menuitem-listing" id="thelist">The List</li>
-          <li class="ddheader color_criticism" id="criticism" >Criticism</li>
-          <li class="ddheader color_features" id="features" >Features</li>
-          <li class="ddheader color_artworld" id="artworld" >Art World</li>
-          <li class="ddheader color_departments" id="departments">Departments</li>
-          <li class="ddheader color_reviewpanel" id="reviewpanel">The Review Panel</li>
-        </ul>
 
-        {{-- DROPDOWNS --}}
-        <div class="ddcontent tl_dropdown" id="thelist-ddcontent" >
-          <a href="https://list.artcritical.com">Week at a Glance</a>
-          <a href="https://list.artcritical.com/current">Current</a>
-          <a href="https://list.artcritical.com/events">Lectures/Events</a>
-        </div>
-      
-        <div class="ddcontent cr_dropdown" id="criticism-ddcontent">
-          <?
-          $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=11&echo=0');
-          $cats = str_replace("<br />","",$cats);
-          echo $cats;
-          ?>
-          <div class="subhead">Regular writers include:</div>
-          <div class="submenu">
-          <?php
-          global $wpdb;
-          $ud = $wpdb->get_results("SELECT * FROM $wpdb->users INNER JOIN $wpdb->usermeta ON ($wpdb->users.ID = $wpdb->usermeta.user_id) WHERE $wpdb->usermeta.meta_key = 'on_menu' AND $wpdb->usermeta.meta_value = 'on' ORDER BY $wpdb->usermeta.meta_value ASC");
-          foreach($ud as $user){
-            $userdata = get_userdata($user->ID);
-            if ($userdata->user_nicename != ""){
-              echo "<a href=\"".App::get_myauthor_link($userdata->user_nicename)."\">".$userdata->display_name."</a>";
-            }
-          }
-          
-          ?>
-          <a href="http://artcritical.com/browse/?tab=byauthor">More...</a>
-          </div>
-        </div>
-        <div class="ddcontent fe_dropdown" id="features-ddcontent" >
-          <?
-          $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=12&echo=0');
-          $cats = str_replace("<br />","",$cats);
-          echo $cats;
-          ?>
-        </div>
-        <div class="ddcontent aw_dropdown" id="artworld-ddcontent">
-          <?
-          $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=13&echo=0');
-          $cats = str_replace("<br />","",$cats);
-          echo $cats;
-          ?>
-        </div>
-        <div class="ddcontent de_dropdown" id="departments-ddcontent">
-          <?
-          $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=14&echo=0');
-          $cats = str_replace("<br />","",$cats);
-          echo $cats;
-          ?>
-          <a href="<?php bloginfo('url')?>/listings-week">The List</a>
-        </div>
-        <div class="ddcontent rp_dropdown" id="reviewpanel-ddcontent">
-                <a href="<?php bloginfo('url')?>/subscribe">Newsletter</a>
-          <?php
-          $args = array(
-            'orderby' => 'date',
-            'order' => 'DESC',
-            'post_type' => 'post',
-            'post_status' => 'publish',
-            'category_name' => 'The Review Panel',
-            'posts_per_page' => 1
-          );
-          $reviewpanel = new WP_Query($args);
-          
-          if ($reviewpanel->have_posts()) : while ($reviewpanel->have_posts()) : $reviewpanel->the_post();
-            ?>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <?php
-    
-          endwhile;
-          endif;
-          ?>
-          
-          <a href="http://artcritical.com/category/departments/the-review-panel">Archive</a>
-          <?= empty($options['next_review_panel_link']) ? '' : '<a href="'.$options['next_review_panel_link'].'">' ;  ?>
-            <?php echo $options['next_review_panel_text']?> - <?php echo $options['next_review_panel']?>
-          <?= empty($options['next_review_panel_link']) ? '' : '</a>' ;  ?>
-        </div>
+          <li class="ddheader color_thelist menuitem-listing" id="thelist">
+            {{-- The List --}}
+            The List
+            <div class="ddcontent tl_dropdown" id="thelist-ddcontent" >
+              <a href="https://list.artcritical.com">Week at a Glance</a>
+              <a href="https://list.artcritical.com/current">Current</a>
+              <a href="https://list.artcritical.com/events">Lectures/Events</a>
+            </div>
+          </li>
+
+          <li class="ddheader color_criticism" id="criticism" >
+            {{-- Criticism --}}
+            Criticism
+            <div class="ddcontent cr_dropdown" id="criticism-ddcontent">
+              @php
+              $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=11&echo=0');
+              $cats = str_replace("<br />","",$cats);
+              @endphp
+              {!! $cats !!}
+
+              <div class="subhead">Regular writers include:</div>
+              <div class="submenu">
+              @php
+              global $wpdb;
+              $ud = $wpdb->get_results("SELECT * FROM $wpdb->users 
+                            INNER JOIN $wpdb->usermeta 
+                            ON ($wpdb->users.ID = $wpdb->usermeta.user_id) 
+                            WHERE $wpdb->usermeta.meta_key = 'on_menu' 
+                            AND $wpdb->usermeta.meta_value = 'on' 
+                            ORDER BY $wpdb->usermeta.meta_value ASC"
+                          );
+              @endphp
+              @foreach($ud as $user)
+                @php $userdata = get_userdata($user->ID) @endphp
+                @if($userdata->user_nicename != "")
+                  <a href="{{App::get_myauthor_link($userdata->user_nicename) }}">{{ $userdata->display_name }}</a>
+                @endif
+              @endforeach
+              <a href="http://artcritical.com/browse/?tab=byauthor">More...</a>
+              </div>
+            </div>
+          </li>
+
+
+          <li class="ddheader color_features" id="features" >
+            {{-- Features --}}
+            Features
+            <div class="ddcontent fe_dropdown" id="features-ddcontent" >
+              @php
+              $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=12&echo=0');
+              $cats = str_replace("<br />","",$cats);
+              @endphp
+              {!! $cats !!}
+            </div>
+          </li>
+
+
+          <li class="ddheader color_artworld" id="artworld" >
+            {{-- Art World --}}
+            Art World
+            <div class="ddcontent aw_dropdown" id="artworld-ddcontent">
+              @php
+              $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=13&echo=0');
+              $cats = str_replace("<br />","",$cats);
+              @endphp
+              {!! $cats !!}
+            </div>
+          </li>
+
+
+          <li class="ddheader color_departments" id="departments">
+            {{-- Departments --}}
+            Departments
+            <div class="ddcontent de_dropdown" id="departments-ddcontent">
+              @php
+              $cats = wp_list_categories('orderby=&title_li=&style=none&hide_empty=0&child_of=14&echo=0');
+              $cats = str_replace("<br />","",$cats);
+              @endphp
+              {!! $cats !!}
+              <a href="<?php bloginfo('url')?>/listings-week">The List</a>
+            </div>
+          </li>
+
+
+          <li class="ddheader color_reviewpanel" id="reviewpanel">
+            {{-- The Review Panel --}}
+            The Review Panel
+            <div class="ddcontent rp_dropdown" id="reviewpanel-ddcontent">
+              <a href="<?php bloginfo('url')?>/subscribe">Newsletter</a>
+        @php
+        $args = array(
+          'orderby' => 'date',
+          'order' => 'DESC',
+          'post_type' => 'post',
+          'post_status' => 'publish',
+          'category_name' => 'The Review Panel',
+          'posts_per_page' => 1
+        );
+        $reviewpanel = new WP_Query($args);
+        @endphp
+        
+        @if($reviewpanel->have_posts())
+          @while ($reviewpanel->have_posts()) @php $reviewpanel->the_post() @endphp
+          <a href="{{ the_permalink() }}">{{ the_title() }}</a>
+          @endwhile
+        @endif
+        
+        <a href="http://artcritical.com/category/departments/the-review-panel">Archive</a>
+        <?= empty($options['next_review_panel_link']) ? '' : '<a href="'.$options['next_review_panel_link'].'">' ;  ?>
+          <?php echo $options['next_review_panel_text']?> - <?php echo $options['next_review_panel']?>
+        <?= empty($options['next_review_panel_link']) ? '' : '</a>' ;  ?>
+      </div>
+          </li>
+        </ul>
         </div>
 
         <div id="rightSection">
