56bd599e7c55c3e9a34f46d9c03bbec5ed6782a0
Francois Huyghe <francois@13milliseconds.com>
2022-02-16 17:58:20 -0500

    Create Cover post type

--- web/app/themes/artcritical2020/app/admin.php
+++ web/app/themes/artcritical2020/app/admin.php
@@ -22,3 +22,22 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+// Older Cover functions
+function cover_post_init() {
+    $args = array(
+        'label' => __('Cover Images'),
+        'singular_label' => __('Cover Image'),
+        'public' => true,
+        'show_ui' => true,
+        '_edit_link' => 'post.php?post=%d',
+        'capability_type' => 'page',
+        'hierarchical' => false,
+        'rewrite' => true,
+        'query_var' => 'cover',
+        'supports' => array('title', 'thumbnail', 'editor', 'excerpt')
+    );
+      
+    register_post_type( 'Cover', $args );
+}
+add_action( 'init', __NAMESPACE__.'\\cover_post_init' );
